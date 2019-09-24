080310eadba5a68650cc0424b73abd95e7c6d2bc
Cindy Sweetpea <cindychau24@me.com>
2019-09-24 14:54:02 -0400

    first commit

---
+++ src/posts/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from tinymce import HTMLField
+
+User = get_user_model()
+
+
+class PostView(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # passing model that does not exist yet, use quotes to ref the name
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    post = models.ForeignKey(
+        "Post", related_name='comments', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # view_count = models.IntegerField(default=0)
+    # comment_count = models.IntegerField(default=0)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    thumbnail = models.ImageField()
+    categories = models.ManyToManyField(Category)
+    featured = models.BooleanField()
+    content = HTMLField()
+    previous_post = models.ForeignKey(
+        'self', related_name='previous', on_delete=models.SET_NULL, blank=True, null=True)
+    next_post = models.ForeignKey(
+        'self', related_name='next', on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={
+            'id': self.id
+        })
+
+    def get_update_url(self):
+        return reverse('post-update', kwargs={
+            'id': self.id
+        })
+
+    def get_delete_url(self):
+        return reverse('post-delete', kwargs={
+            'id': self.id
+        })
+
+    @property
+    def get_comments(self):
+        return self.comments.all().order_by('-timestamp')
+
+    @property
+    def view_count(self):
+        # return the count in postview that matches self.
+        return PostView.objects.filter(post=self).count()
+
+    @property
+    def comment_count(self):
+        # return the count of comments that matches self.
+        return Comment.objects.filter(post=self).count()
